3f36d604a6b6bda3018f17d52d0fc068a6c47e91
Julien Marusi <julien.marusi@bbee.fr>
2022-02-09 19:38:52 +0100

    bcd

--- prg/scroller/test.asm
+++ prg/scroller/test.asm
@@ -3,7 +3,7 @@
 :BasicUpstart2(start)
 
 start: {
-    lda $DD00 // 081C
+    lda $DD00
     and #%11111100
     ora #%00000010 // VIC-II bank 2
     sta $dd00
@@ -15,7 +15,7 @@ start: {
     sta $d018
 
     // clear out two last rows of the bitmap
-    lda #0 //0833
+    lda #0
     .for (var i = 0; i < 40; i++) {
         sta colora + 40*23 + i
         sta colorb + 40*23 + i
@@ -26,6 +26,9 @@ infloop:
 }
 
 .align 64
+ *=$4000 "reserve VIC memory - don't let this overlap any other segment" virtual
+pad: .fill $1000,0
+
 * = $6000 "image"
 gfx:
 .import source "titleimage.txt"
